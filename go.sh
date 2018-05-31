c6850bcb4fae306addf1993c7a60e8aadc5c13cd
wdzajicek <tig.welder4@gmail.com>
2018-05-31 14:30:34 -0500

    Update shell script

--- go.sh
+++ go.sh
@@ -1,14 +1,27 @@
+
+echo "Creating local Gemfile from Gemfile.txt"
+
+sleep .5
+
 # Create local Gemfile from Gemfile.txt
 cp Gemfile.txt Gemfile
 
-echo "Creating local Gemfile from Gemfile.txt"
+echo "running npm isntall"
+
+sleep .25
+
+echo "NPM will return permission errors if you do not have NVM - https://github.com/creationix/nvm"
+
+sleep .5
 
 # NPM install
 npm install
 
-echo "running npm isntall"
+echo "running bundle install"
+
+sleep .5
 
 # Bundle install
 bundle install
 
-echo "running bundle install"
+echo "Installation Complete!"
