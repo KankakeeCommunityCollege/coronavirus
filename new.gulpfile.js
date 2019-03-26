5659ad224fa41c8faabd6603c6940a79e0a99005
wdzajicek <tig.welder4@gmail.com>
2019-03-26 15:44:18 -0500

    Make gulp 4 work!

--- new.gulpfile.js
+++ new.gulpfile.js
@@ -1,57 +1,90 @@
-const fs = require('fs');
-const yargs = require('yargs');
-const yaml = require('js-yaml');
+const { series, parallel, watch } = require('gulp');
+const gulp = require('gulp');
 const del = require('del');
-const browserSync = require('browser-sync');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const spawn = require('cross-spawn');
+const yargs = require('yargs');
 const sitemap = require('gulp-sitemap');
 const gulpif = require('gulp-if');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync');
+const config = require('./gulp-tasks/loadGulpConfig'); // require 
 
-//const requireDir = require('require-dir');
-//requireDir('./gulp-tasks');
-
-const PRODUCTION = !!(yargs.argv.production);
-
-const { series, parallel } = require('gulp');
-
+function clean(done) {
+  /*
 function loadConfig() {
-  const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
-  return yaml.load(ymlFile);
-}
-const config = loadConfig();
-module.exports = config;
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;*/
 
-//Delete the _site dir
-function clean(cb) {
+
+  // Clean stuff
   del(config.clean);
-  cb();
+  done();
 }
 
-// run jekyll's build command
 function jekyllBuild(done) {
+  /*
+function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;*/
+
+
+
   browserSync.notify(config.jekyll.notification);
   return spawn('jekyll', ['build'], {
     stdio: 'inherit'
   })
-    .on('close', done);
+  .on('close', done);
 }
 
-// create the sitemap.xml
-function sitemap() {
+function gulpSitemap(done) {
+  const PRODUCTION = !!(yargs.argv.production); // Run things that say 'PRODCUTION' on production builds only ($ gulp --production)
+
+  /*
+  function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;  */
+
+
+
   gulp.src((config.sitemap.src), {
     read: false
   })
-  .pipe(sitemap({
-    siteUrl: (config.sitemap.siteUrl),
-  }))
-  .pipe(gulpif(PRODUCTION, gulp.dest('./')))
-  .pipe(gulpif(PRODUCTION, gulp.dest('./_site')));
+    .pipe(sitemap({
+      siteUrl: (config.sitemap.siteUrl),
+    }))
+    .pipe(gulpif(PRODUCTION, gulp.dest('./')))
+    .pipe(gulpif(PRODUCTION, gulp.dest('./_site')));
+  done();
 }
 
-function mainSass() {
+function mainScss() {
+  const PRODUCTION = !!(yargs.argv.production); // Run things that say 'PRODCUTION' on production builds only ($ gulp --production)
+
+  /*
+function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;*/
+
+
+
   return gulp.src(config.sass.src)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError)) // errors shown in terminal for when you screw up your SASS
@@ -63,26 +96,60 @@ function mainSass() {
     .pipe(browserSync.stream());
 }
 
-function cmsSass() { // compiles stylesheet used for custom CloudCannon options
-  return gulp.src(config.contentSass.src)
+function cmsScss() {
+  const PRODUCTION = !!(yargs.argv.production); // Run things that say 'PRODCUTION' on production builds only ($ gulp --production)
+
+  /*
+  function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;  */
+
+
+
+  return gulp.src(config.cmsScss.src)
     .pipe(sourcemaps.init())
-    .pipe(contentSass().on('error', contentSass.logError)) // errors shown in terminal for when you screw up your SASS
-    .pipe(autoprefixer(config.contentSass.compatibility)) // Automatically prefix any CSS that is not compatible with the browsers defined in the gulpconfig
+    .pipe(sass().on('error', sass.logError)) // errors shown in terminal for when you screw up your SASS
+    .pipe(autoprefixer(config.cmsScss.compatibility)) // Automatically prefix any CSS that is not compatible with the browsers defined in the gulpconfig
     .pipe(gulpif(PRODUCTION, cssnano({ zindex: false }))) // {zindex:false} to prevent override of z-index values -- higher z-index's needed to bring objects above bootstrap's default z-index values
     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-    .pipe(gulp.dest(config.contentSass.dest.jekyllRoot))
-    .pipe(gulp.dest(config.contentSass.dest.buildDir))
+    .pipe(gulp.dest(config.cmsScss.dest.jekyllRoot))
+    .pipe(gulp.dest(config.cmsScss.dest.buildDir))
     .pipe(browserSync.stream());
 }
 
 function copy() {
+  const PRODUCTION = !!(yargs.argv.production); // Run things that say 'PRODCUTION' on production builds only ($ gulp --production)
+
+  /*
+function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;*/
+
+
+
   browserSync.notify(config.copy.notification);
   return gulp.src(config.copy.assets)
     .pipe(gulpif(PRODUCTION, imagemin())) // compresses images when gulp --production is run.
     .pipe(gulp.dest(config.copy.dist));
 }
 
-function browserSync(done) {
+function browserSyncInit(done) {
+  /*
+function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;*/
+
+
+
   browserSync.init({
     notify: config.browsersync.notify,
     open: config.browsersync.open,
@@ -96,31 +163,70 @@ function browserSync(done) {
   done();
 }
 
-function watch() {
-  gulp.watch(config.watch.pages, ['build', browserSync.reload]); // Watch for new pages and changes.
-  gulp.watch(config.watch.sass, ['sass']); // SASS/SCSS changes
-  gulp.watch(config.watch.sass, ['contentSass']); // SASS/SCSS changes
-  gulp.watch(config.watch.images, ['copy', browserSync.reload]); // Watch for new static assets like images
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
 }
 
-// export tasks
-exports.default = build;
-exports.clean = clean;
-exports.jekyllBuild = jekyllBuild;
-exports.mainSass = mainSass;
-exports.cmsSass = cmsSass;
-exports.copy = copy;
-exports.watch = watch;
+function watchFiles() {
+  /*
+function loadConfig() {
+    const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
+    return yaml.load(ymlFile);
+  }
+  const config = loadConfig();
+  module.exports = config;*/
 
-exports.build = series(
+
+
+  watch(
+    config.watch.pages,
+    series(
+      build,
+      browserSyncReload
+    )
+  ); // Watch for new pages and changes.
+
+  watch(
+    config.watch.sass,
+    mainScss
+  ); // SASS/SCSS changes
+
+  watch(
+    config.watch.sass,
+    cmsScss
+  ); // SASS/SCSS changes
+  watch(
+    config.watch.images,
+    series(
+      copy,
+      browserSyncReload // Watch for new static assets like images
+    )
+  );
+}
+
+// More complex tasks go like this:
+const build = series(
   clean,
   jekyllBuild,
   parallel(
-    sitemap,
-    mainSass,
-    cmsSass,
+    gulpSitemap,
+    mainScss,
+    cmsScss,
     copy
   ),
-  browserSync,
-  watch
+);
+
+exports.build = build;
+//uncommenting these would make it so you can call `$ gulp watch` or `$ gulp browserSync` from terminal:
+//exports.browserSync = browserSync;
+//exports.watchFiles = watchFiles;
+
+exports.default = series(
+  build,
+  parallel(
+    browserSyncInit,
+    watchFiles
+  )
 );
