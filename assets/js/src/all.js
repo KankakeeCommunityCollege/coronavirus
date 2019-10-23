448f6495d710117cc8b8bad30a4e205b27463a82
wdzajicek <tig.welder4@gmail.com>
2019-10-23 14:53:41 -0500

    Create new bundle & main.css files using new browserlist IE settings...
    1. Change the browserlist IE compatibility settings to 'ie>=11'
    2. Run a build (`npm run production`)
    3. Resulting JS,CSS, & hash files were created with the new compatibility settings (auto-prefixer also runs off
    the browserlist settings in package.json).

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,2 +1,6 @@
-import './test.js';
+import test from './test.js';
 //import './babelTest.js';
+
+document.addEventListener('DOMContentLoaded', function() {  
+  test();
+});
--- assets/js/src/test.js
+++ assets/js/src/test.js
@@ -1,1 +1,10 @@
 // THIS IS A TEST
+function test() {
+  //const array = [1,2,3,4];
+
+  //Array.from(array).forEach(($item) => {
+    //console.log($item);
+  //})
+}
+
+export default test;
