8b72b892e7cece03a8cbe226ac62d5050717f568
wdzajicek <tig.welder4@gmail.com>
2020-03-12 19:06:02 -0500

    Create custom JS to toggle button text.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,6 +1,8 @@
 import test from './test.js';
+import toggleButton from './toggleButton.js';
 //import './babelTest.js';
 
-document.addEventListener('DOMContentLoaded', function() {  
+document.addEventListener('DOMContentLoaded', function() {
   test();
+  toggleButton();
 });
