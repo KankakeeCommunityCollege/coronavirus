aa73a9f53b69ddaad40d8b0329d187416c3c6fbb
wdzajicek <tig.welder4@gmail.com>
2021-10-05 14:48:50 -0500

    Remove gulp and upgrade project.

---
+++ _plugins/last-mod-time.rb
@@ -0,0 +1,25 @@
+Jekyll::Hooks.register :pages, :pre_render do |page|
+  
+  if File.exists?(page.path)
+
+    # get the page's last modified time
+    modification_time = File.mtime( page.path )
+
+    # inject modification_time into page's data.
+    page.data['last-mod-time'] = modification_time
+  end
+
+end
+
+Jekyll::Hooks.register :posts, :pre_render do |post|
+  
+  if File.exists?(post.path)
+
+    # get the post's last modified time
+    modification_time = File.mtime( post.path )
+
+    # inject modification_time into post's data.
+    post.data['last-mod-time'] = modification_time
+  end
+
+end
